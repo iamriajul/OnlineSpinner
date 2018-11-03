33955a72455a2e2e6e9fea66377515ae24de1866
Riajul <kmriajulislami@gmail.com>
2018-11-03 08:22:49 +0600

    added layout_height attribute support on spinner, and added more example of usage in app module

--- app/src/main/java/io/github/iamriajul/onlinespinnerexample/MainActivity.kt
+++ app/src/main/java/io/github/iamriajul/onlinespinnerexample/MainActivity.kt
@@ -4,9 +4,11 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import io.github.iamriajul.onlinespinner.ActivityWithOnlineSpinner
 import kotlinx.android.synthetic.main.activity_main.*
+import org.json.JSONArray
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity(), ActivityWithOnlineSpinner {
-    override var totalFieldsCount: Int = 2 // Total OnlineSpinner fields in this activity is using
+    override var totalFieldsCount: Int = 3 // Total OnlineSpinner fields in this activity is using
     override var fieldsLoaded: Int = 0 // it should be 0
 
     override fun hideLoader() {
@@ -25,5 +27,8 @@ class MainActivity : AppCompatActivity(), ActivityWithOnlineSpinner {
         // Change dataUrl with your real data url
         example.load(this, "http://example.com/country/all", 5)
         example2.load(this, "http://example.com/language/all", 3, "lang")
+        val dummyJsonArray = JSONArray("[{id:1, name: 'Item 1'}, {id:2, name: 'Item 2'}, {id:3, name: 'Item 3'}]")
+        example3.load(this, dummyJsonArray)
+        example4.load(this, dummyJsonArray)
     }
 }
--- onlinespinner/src/main/java/io/github/iamriajul/onlinespinner/OnlineSpinner.kt
+++ onlinespinner/src/main/java/io/github/iamriajul/onlinespinner/OnlineSpinner.kt
@@ -6,6 +6,7 @@ import android.util.AttributeSet
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
+import android.view.Window
 import android.widget.*
 import com.github.kittinunf.fuel.android.extension.responseJson
 import com.github.kittinunf.fuel.core.Request
@@ -48,6 +49,10 @@ class OnlineSpinner : LinearLayout {
             AppCompatSpinner(context)
         }
         addView(spinner)
+
+        val layout_height = attrs?.getAttributeIntValue("http://schemas.android.com/apk/res/android", "layout_height", LayoutParams.WRAP_CONTENT)
+        val layoutParams = LayoutParams(LayoutParams.MATCH_PARENT, layout_height ?: LayoutParams.WRAP_CONTENT)
+        spinner.layoutParams = layoutParams
 
         isOptional = ta.getBoolean(R.styleable.OnlineSpinner_isOptional, false)
 
